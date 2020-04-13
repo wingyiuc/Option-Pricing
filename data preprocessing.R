b594f4c1470babbcd9a17b3a5e40c22d40bfaef4
Yoyo Chan <46639292+wingyiuc@users.noreply.github.com>
2020-04-13 22:58:46 +0800

    Update data preprocessing.R

--- data preprocessing.R
+++ data preprocessing.R
@@ -1,35 +1,39 @@
 #######################################
-### Code for pricing multiple stocks
+### Code for data preprocessing
 # How to use: drop the historical price rds files into "Price_data" folder
-# Run the code and get the basket option price
-# For new data download from HKEX website:
-# drop the downloaded excel files into "Price_data" folder
-# uncomment and run the "Save excel as rds" section
+# Run the code and get the processed rds files
 #######################################
-
+### Load packages
 library(data.table)
 library(readxl)
 library(stringr)
-library(doParallel)
-library(doRNG)
-registerDoParallel(cores=detectCores())
-#####
-# Directory setup
+#######################################
+### Directory setup
+# ***User edit area***
 mainDir = "E:/Yoyo Chan/Documents/FINA4354 Financial engineering/Group project/"
 setwd(mainDir)
 dataDir = paste(mainDir , "Price_data/", sep="")
-#####
 
 #######################################
-## Save excel as rds 
+## Save excel as rds for pricing
 files = list.files(dataDir,pattern = ".xlsx")
 for (file in files) {
   df = read_excel(paste(dataDir,file,sep=""))
   filename = strsplit(file,".xlsx")[1]
   setDT(df)
-  df = df[Time > "2018/12/31" & Time<"2020/01/01",]
-  saveRDS(df, paste(dataDir,filename,".rds",sep="") )
+  df = df[Time > "2018/03/31" & Time<"2019/04/02",]
+  saveRDS(df, paste(dataDir,filename,"_pricing.rds",sep="") )
 }
 
 #######################################
+## Save excel as rds for backtesting
+files = list.files(dataDir,pattern = ".xlsx")
+for (file in files) {
+  df = read_excel(paste(dataDir,file,sep=""))
+  filename = strsplit(file,".xlsx")[1]
+  setDT(df)
+  df = df[Time > "2018/03/31" & Time<"2020/04/02",]
+  saveRDS(df, paste(dataDir,filename,"_backtest.rds",sep="") )
+}
 
+#######################################
