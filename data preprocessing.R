e811de3843907f553876c0c80cd2acad3697a487
wingyiuc <wingyiuc0202@gmail.com>
2020-04-15 14:28:03 +0800

    updated with preprocessing function added

--- data preprocessing.R
+++ data preprocessing.R
@@ -7,12 +7,28 @@
 library(data.table)
 library(readxl)
 library(stringr)
+library(dplyr)
 #######################################
 ### Directory setup
 # ***User edit area***
 mainDir = "C:/Users/kenneth.DESKTOP-DIPDF8F/Option-Pricing/"
 setwd(mainDir)
 dataDir = paste(mainDir , "data/", sep="")
+
+#######################################
+# Functions
+preprocess.df = function(df){
+  
+  # Input: dataframe from rds files
+  # Output: datatable with date, Closed_Price and ret (return)
+  
+  setDT(df)
+  names(df) = str_replace_all(names(df)," ","_")
+  df = df[,list(date=Time,Closed_Price= Closed_Price %>% as.character %>% as.numeric),]
+  df = df[,ret:= log(Closed_Price)-log(shift(Closed_Price,type="lag",n=1)),]
+  df = na.omit(df)
+  return(df)
+}
 
 #######################################
 ## Save excel as rds for pricing
@@ -37,3 +53,14 @@ for (file in files) {
 }
 
 #######################################
+## Save csv from Yahoo Finance as rds for pricing
+files = list.files(dataDir,pattern = ".csv")
+for (file in files) {
+  df = read.csv(paste(dataDir,file,sep=""))
+  filename = strsplit(file,".csv")[1]
+  setDT(df)
+  df = df[,.(Time=format(as.Date(Date),"%Y/%m/%d"), Closed_Price=Adj.Close)]
+  df = df[Time > "2018/03/31" & Time<"2019/04/02",]
+  df = preprocess.df(df)
+  saveRDS(df, paste(dataDir,filename,"_pricing.rds",sep=""))
+}
