53a0e0c0d0ea0d27720d29b7d2db32d1a136000a
wingyiuc <wingyiuc0202@gmail.com>
2020-04-18 13:09:54 +0800

    organized data file

--- data preprocessing.R
+++ data preprocessing.R
@@ -1,7 +1,8 @@
 #######################################
 ### Code for data preprocessing
-# How to use: drop the historical price rds files into "data" folder
+# How to use: drop the historical price excel/csv files into "data" folder
 # Run the code and get the processed rds files
+# Expected ouput: pricing/ backtest rds files with columns "date", "Closd_price" and "ret"
 #######################################
 ### Load packages
 library(data.table)
@@ -31,7 +32,7 @@ preprocess.df = function(df){
 }
 
 #######################################
-## Save excel as rds for pricing
+## Save excel from HKEX as rds for pricing
 files = list.files(dataDir,pattern = ".xlsx")
 for (file in files) {
   df = read_excel(paste(dataDir,file,sep=""))
@@ -43,7 +44,7 @@ for (file in files) {
 }
 
 #######################################
-## Save excel as rds for backtesting
+## Save excel from HKEX as rds for backtesting
 files = list.files(dataDir,pattern = ".xlsx")
 for (file in files) {
   df = read_excel(paste(dataDir,file,sep=""))
@@ -62,7 +63,7 @@ for (file in files) {
   filename = strsplit(file,".csv")[1]
   setDT(df)
   df = df[,.(Time=format(as.Date(Date),"%Y/%m/%d"), Closed_Price=Adj.Close)]
-  df = df[Time > "2018/03/31" & Time<"2019/04/02",]
+  df = df[Time > "2017/04/01" & Time<"2018/04/01",]
   df = preprocess.df(df)
   saveRDS(df, paste(dataDir,filename,"_pricing.rds",sep=""))
 }
@@ -75,7 +76,7 @@ for (file in files) {
   filename = strsplit(file,".csv")[1]
   setDT(df)
   df = df[,.(Time=format(as.Date(Date),"%Y/%m/%d"), Closed_Price=Adj.Close)]
-  df = df[Time > "2018/03/31" & Time<"2020/04/02",]
+  df = df[Time > "2017/04/01" & Time<"2019/04/01",]
   df = preprocess.df(df)
   saveRDS(df, paste(dataDir,filename,"_backtest.rds",sep=""))
 }
